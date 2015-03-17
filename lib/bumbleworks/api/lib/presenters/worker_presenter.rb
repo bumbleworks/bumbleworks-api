a83f2986539e10495c667875ad6ffbd4651d96b6
Ravi Gadad <ravi@renewfund.com>
2015-03-17 04:40:38 -0700

    Add uptime in words to Workers API

--- lib/bumbleworks/api/lib/presenters/worker_presenter.rb
+++ lib/bumbleworks/api/lib/presenters/worker_presenter.rb
@@ -14,11 +14,16 @@ module Bumbleworks
           :updated_at => presented.updated_at,
           :worker_class => presented.worker_class_name,
           :uptime => presented.uptime,
+          :uptime_in_words => uptime_in_words,
           :processed_last_minute => presented.processed_last_minute,
           :wait_time_last_minute => presented.wait_time_last_minute,
           :processed_last_hour => presented.processed_last_hour,
           :wait_time_last_hour => presented.wait_time_last_hour
         }
+      end
+
+      def uptime_in_words
+        TimeSupport.seconds_to_units_in_words(presented.uptime)
       end
     end
   end
--- spec/lib/presenters/worker_presenter_spec.rb
+++ spec/lib/presenters/worker_presenter_spec.rb
@@ -6,6 +6,7 @@ describe Bumbleworks::Api::WorkerPresenter do
     it "returns hash representation of worker" do
       Bumbleworks.start_worker!
       allow(presented).to receive(:uptime).and_return(:fake_uptime)
+      allow(subject).to receive(:uptime_in_words).and_return(:fake_words)
       expect(subject.to_hash).to eq({
         :id => presented.id,
         :pid => presented.pid,
@@ -18,6 +19,7 @@ describe Bumbleworks::Api::WorkerPresenter do
         :updated_at => presented.updated_at,
         :worker_class => presented.worker_class_name,
         :uptime => :fake_uptime,
+        :uptime_in_words => :fake_words,
         :processed_last_minute => presented.processed_last_minute,
         :wait_time_last_minute => presented.wait_time_last_minute,
         :processed_last_hour => presented.processed_last_hour,
